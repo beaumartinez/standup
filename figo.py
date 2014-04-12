71989ec03129fdd7d599baee02c7d7792886d57b
Beau <beau@beaumartinez.com>
2014-04-12 11:26:19 +0100

    Parse tickets and ticket notes
    For less boilerplate.

--- figo.py
+++ figo.py
@@ -3,12 +3,16 @@ from __future__ import print_function
 import logging
 from collections import defaultdict
 from datetime import datetime
+from datetime import timedelta
 from pprint import pformat
 from sys import argv
 from sys import exit
 from sys import stderr
 
 from requests import Session
+
+from parser import parse_ticket
+from parser import parse_ticket_note
 
 
 log = logging.getLogger(__name__)
@@ -23,7 +27,7 @@ class Codebase(object):
         self.username = username
         self.key = key
 
-        date = datetime.utcnow()
+        date = datetime.utcnow() - timedelta(days=1)
         self.date = date.strftime('%Y-%m-%d')
 
         self.session = self._create_session()
@@ -70,41 +74,50 @@ class Codebase(object):
 
         self.tickets = tickets
 
+    def _parse_tickets(self):
+        self.tickets = map(parse_ticket, self.tickets)
+
     def _build_ticket_note_urls(self):
         for ticket in self.tickets:
-            ticket['ticket']['ticket_note_url'] = (
+            ticket['ticket_note_url'] = (
                 'https://api3.codebasehq.com/locus/tickets/{}/notes.json'
-            ).format(ticket['ticket']['ticket_id'])
+            ).format(ticket['ticket_id'])
 
     def _get_ticket_notes(self):
         for ticket in self.tickets:
-            log.debug('Getting notes for ticket {}.'.format(ticket['ticket']['ticket_id']))
+            log.debug('Getting notes for ticket {}.'.format(ticket['ticket_id']))
 
-            response = self.session.get(ticket['ticket']['ticket_note_url'])
+            response = self.session.get(ticket['ticket_note_url'])
 
-            ticket['ticket']['ticket_notes'] = response.json()
+            ticket['ticket_notes'] = response.json()
+
+    def _parse_ticket_notes(self):
+        for ticket in self.tickets:
+            ticket['ticket_notes'] = map(parse_ticket_note, ticket['ticket_notes'])
 
     def _filter_todays_ticket_notes(self):
         for ticket in self.tickets:
-            ticket['ticket']['ticket_notes'] = filter(
-                lambda x: x['ticket_note']['created_at'].startswith(self.date), ticket['ticket']['ticket_notes']
+            ticket['ticket_notes'] = filter(
+                lambda x: x['created_at'].startswith(self.date), ticket['ticket_notes']
             )
 
     def _group_user_ticket_notes(self):
         user_ticket_notes = defaultdict(set)
 
         for ticket in self.tickets:
-            for note in ticket['ticket']['ticket_notes']:
-                username = self.user_lookup[note['ticket_note']['user_id']]
+            for note in ticket['ticket_notes']:
+                username = self.user_lookup[note['user_id']]
 
-                user_ticket_notes[username].add('{}: {}'.format(ticket['ticket']['ticket_id'], ticket['ticket']['summary']))
+                user_ticket_notes[username].add('{}: {}'.format(ticket['ticket_id'], ticket['summary']))
 
         self.user_ticket_notes = dict(user_ticket_notes)
 
     def get_own_ticket_notes(self):
         self._get_tickets()
+        self._parse_tickets()
         self._build_ticket_note_urls()
         self._get_ticket_notes()
+        self._parse_ticket_notes()
         self._filter_todays_ticket_notes()
         self._get_users()
         self._build_user_id_lookup()
