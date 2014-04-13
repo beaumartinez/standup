87ac6cd66b59d52aa287266cbdaf7ff4e0ec40b1
Beau <beau@beaumartinez.com>
2014-04-13 23:05:20 +0100

    Better help

--- bin/codebase.py
+++ bin/codebase.py
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     parser.add_argument('username', help='API username')
     parser.add_argument('key', help='API key')
     parser.add_argument('project', help='project name')
-    parser.add_argument('--days-ago', default=None, help="show DAYS_AGO's activity", type=int)
+    parser.add_argument('--days-ago', default=None, help="show DAYS_AGO days ago's activity", type=int)
     parser.add_argument('--debug', action='store_true')
 
     args = parser.parse_args()
