d42286f0222e8c7b9059a9feed77cb29381c62de
Beau <beau@beaumartinez.com>
2014-04-13 23:01:30 +0100

    Move to bin

--- bin/codebase.py
+++ bin/codebase.py
@@ -1,24 +1,26 @@
+#! /usr/bin/env python2
+
 from __future__ import print_function
 
 from argparse import ArgumentParser
-from sys import argv
-from sys import exit
-from sys import stderr
+from sys import path
 import logging
 
-from . import Codebase
+path.insert(0, '..')
+
+from codebase import Codebase
 
 
 if __name__ == '__main__':
-    parser = ArgumentParser(description='Print today\'s activity on Codebase.')
-    parser.add_argument('username')
-    parser.add_argument('key')
-    parser.add_argument('project')
-    parser.add_argument('--days-ago', default=None, type=int)
+    parser = ArgumentParser(description="Print today's activity on Codebase.")
+    parser.add_argument('username', help='API username')
+    parser.add_argument('key', help='API key')
+    parser.add_argument('project', help='project name')
+    parser.add_argument('--days-ago', default=None, help="show DAYS_AGO's activity", type=int)
     parser.add_argument('--debug', action='store_true')
 
     args = parser.parse_args()
-    
+
     if args.debug:
         logger = logging.getLogger('codebase')
 
