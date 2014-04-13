7391a758f915ba193b7481804b4bada3d569fda2
Beau <beau@beaumartinez.com>
2014-04-13 22:46:51 +0100

    argparse

--- codebase/__main__.py
+++ codebase/__main__.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 
+from argparse import ArgumentParser
 from sys import argv
 from sys import exit
 from sys import stderr
@@ -9,23 +10,21 @@ from . import Codebase
 
 
 if __name__ == '__main__':
-    logger = logging.getLogger('codebase')
-    # logger.setLevel(logging.DEBUG)
+    parser = ArgumentParser(description='Print today\'s activity on Codebase.')
+    parser.add_argument('username')
+    parser.add_argument('key')
+    parser.add_argument('project')
+    parser.add_argument('--days-ago', default=None, type=int)
+    parser.add_argument('--debug', action='store_true')
 
-    try:
-        username, key, project = argv[1], argv[2], argv[3]
-    except IndexError:
-        print('Please pass your username, key, and project', file=stderr)
-        exit(1)
+    args = parser.parse_args()
+    
+    if args.debug:
+        logger = logging.getLogger('codebase')
 
-    try:
-        days_ago = argv[4]
-    except IndexError:
-        days_ago = None
-    else:
-        days_ago = int(days_ago)
+        logger.setLevel(logging.DEBUG)
 
-    codebase = Codebase(username, key, project, days_ago=days_ago)
+    codebase = Codebase(args.username, args.key, args.project, days_ago=args.days_ago)
     codebase.get_tickets()
 
     for user in sorted(codebase.user_ticket_lookup):
