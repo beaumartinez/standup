47fcca8db4e86b594100826256558505cac9f63c
Beau <beau@beaumartinez.com>
2014-04-14 09:55:48 +0100

    Short flags

--- bin/codebase.py
+++ bin/codebase.py
@@ -14,8 +14,8 @@ if __name__ == '__main__':
     parser.add_argument('username', help='API username')
     parser.add_argument('key', help='API key')
     parser.add_argument('project', help='project name')
-    parser.add_argument('--user', default='', help="only print USER's activity")
-    parser.add_argument('--days-ago', default=None, help="show DAYS_AGO days ago's activity", type=int)
+    parser.add_argument('-u', '--user', default='', help="only print USER's activity")
+    parser.add_argument('-d', '--days-ago', default=None, help="show DAYS_AGO days ago's activity", type=int)
     parser.add_argument('--debug', action='store_true')
 
     args = parser.parse_args()
