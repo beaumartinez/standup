06fd632f41c856681482ea4444cd8a4de832c746
Beau <beau@beaumartinez.com>
2014-04-14 09:35:45 +0100

    --user

--- bin/codebase.py
+++ bin/codebase.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     parser.add_argument('username', help='API username')
     parser.add_argument('key', help='API key')
     parser.add_argument('project', help='project name')
+    parser.add_argument('--user', default='', help="only print USER's activity")
     parser.add_argument('--days-ago', default=None, help="show DAYS_AGO days ago's activity", type=int)
     parser.add_argument('--debug', action='store_true')
 
@@ -28,8 +29,9 @@ if __name__ == '__main__':
     codebase.get_tickets()
 
     for user in sorted(codebase.user_ticket_lookup):
-        print(user)
+        if user.startswith(args.user):
+            print(user)
 
-        tickets = sorted(codebase.user_ticket_lookup[user])
-        for ticket in tickets:
-            print('    {}'.format(ticket))
+            tickets = sorted(codebase.user_ticket_lookup[user])
+            for ticket in tickets:
+                print('    {}'.format(ticket))
