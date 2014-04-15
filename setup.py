0116d093ca9df3d38e8eecc8514ece7cb9c98260
Beau <beau@beaumartinez.com>
2014-04-15 16:01:05 +0200

    Better setup.py

--- setup.py
+++ setup.py
@@ -5,11 +5,23 @@ from setuptools import setup
 
 setup(
     author='Beau Martinez',
+    author_email='beau@beaumartinez.com',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Programming Language :: Python :: 3.4',
+    ],
     description='What the FUCK did I do today',
-    install_requires=['requests>=2.2.1'],
+    install_requires=[
+        'requests>=2.2.1',
+    ],
     name='standup',
-    packages=['standup', 'standup.codebase'],
-    scripts=['bin/standup_codebase.py'],
+    packages=[
+        'standup',
+        'standup.codebase',
+    ],
+    scripts=[
+        'bin/standup_codebase.py',
+    ],
     url='http://github.com/beaumartinez/standup/',
     version='0.1',
 )
