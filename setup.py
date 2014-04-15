e05b4f91bfdb9b3acacd99cec02bd6b68ee39979
Beau <beau@beaumartinez.com>
2014-04-15 14:28:06 +0200

    No email or licence lel

--- setup.py
+++ setup.py
@@ -5,9 +5,7 @@ from distutils.core import setup
 
 setup(
     author='Beau Martinez',
-    author_email='beau@beaumartinez.com',
     description='What the FUCK did I do today',
-    license='WTFPL',
     name='standup',
     packages=['standup', 'standup.codebase'],
     scripts=['bin/codebase_standup.py'],
