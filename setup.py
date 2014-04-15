ff9292b97b2baa0001fb18ce4cf8f78640f98025
Beau <beau@beaumartinez.com>
2014-04-15 12:36:29 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+from distutils.core import setup
+
+
+setup(
+    author='Beau Martinez',
+    author_email='beau@beaumartinez.com',
+    description='What the FUCK did I do today',
+    license='WTFPL',
+    name='standup',
+    packages=['codebase'],
+    scripts=['bin/standup.py'],
+    url='http://github.com/beaumartinez/standup/',
+    version='0.1',
+)
